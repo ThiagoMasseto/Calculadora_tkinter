b35bd5498e5ec24dfc45ebaf1ecff9279d2192da
ThiagoMasseto <thimasseto@gmail.com>
2026-06-10 21:38:12 -0300

    refatorando a logica, para utilizar loop de repetição ao inves de criar botao por botao.

--- app.py
+++ app.py
@@ -11,17 +11,15 @@ visor = tk.Entry(janela) # Cria o visor da calculadora
                          # onde os números serão exibidos
 visor.pack() # Exibe o visor na tela
 
-# Cria um botão com o texto "1"
-botao1 = tk.Button( 
+# loop de repeticao, para criação dos botões.
+for numero in range(10):
+    botao = tk.Button(
+        janela,
+        text=str(numero),
+        command=lambda n=numero: funcoes.clicar(visor, str(n))
+    )
 
-    janela,  # Define a janela onde o botão será colocado
-    text="1", # Texto exibido no botão
-    command=lambda: funcoes.clicar(visor, "1")  # Quando clicar no botão:
-                                                # chama a função clicar()
-                                                # passando o visor e o número 1
-)
-
-botao1.pack() # Exibe o botão na tela
+botao.pack() # Exibe o botão na tela
 
 janela.mainloop() # Mantém a janela aberta e esperando eventos
                   # como cliques e teclas
