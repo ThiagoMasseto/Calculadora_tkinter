45e8d485712de0649a0573ffac5c2bcc3541799f
ThiagoMasseto <thimasseto@gmail.com>
2026-06-10 21:10:38 -0300

    Adicionando alguns ajustes e aperfeiçoamentos na janela.

--- app.py
+++ app.py
@@ -3,7 +3,21 @@ import tkinter as tk
 janela = tk.Tk()
 janela.title("Calculadora")
 
-visor = tk.Entry(janela)
-visor.pack()
+# largura x altura
+janela.geometry("300x400") # 300 pixels de largura
+                           # 400 pixels de altura
+
+
+visor = tk.Entry(
+    janela,
+    font = ("Arial", 20) # Aumenta o tamanho da letra do visor.
+    )
+
+visor.pack(
+    padx=10, # Espaçamento horizontal.
+    pady=10, # Espaçamento vertical.
+    fill="x"
+)
+
 
 janela.mainloop()
